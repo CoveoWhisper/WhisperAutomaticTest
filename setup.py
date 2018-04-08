08c9d767f91b69eb57b49919085b39411baa4c63
= <webf0x@hotmail.com>
2018-04-08 16:24:13 -0400

    Setup project with Tox

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='WhisperAutomaticTest',
+    version='0.1dev',
+    packages=['whisper_automatic_test'],
+    license='MIT',
+    long_description='Whisper automatic test'
+)
