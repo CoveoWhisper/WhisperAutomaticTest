66eb606f4be07d441692fc2da628869c8aeee42c
Maxime Dupuis <webf0x@hotmail.com>
2018-04-11 11:18:00 -0400

    Convert Whisper response to suggestions (#3)
    * Parse space separated URL's in scenarios
    
    * Convert whisper response to suggestions

--- test/test_scenario_reader.py
+++ test/test_scenario_reader.py
@@ -1,27 +1,30 @@
 import unittest
 from whisper_automatic_test.scenario_reader import get_scenarios_from_csv_file
 
-scenario_file_path = 'test/resources/test_scenario.csv'
+SCENARIO_FILE_PATH = 'test/resources/test_scenario.csv'
 
 
 class TestScenarioReader(unittest.TestCase):
 
     def test_read_scenarios_from_csv_file(self):
-        scenarios = get_scenarios_from_csv_file(scenario_file_path)
+        scenarios = get_scenarios_from_csv_file(SCENARIO_FILE_PATH)
         self.assertEquals(2, len(scenarios))
 
     def test_scenario_with_single_request(self):
-        scenarios = get_scenarios_from_csv_file(scenario_file_path)
+        scenarios = get_scenarios_from_csv_file(SCENARIO_FILE_PATH)
         scenario = scenarios[0]
         requests = scenario.get_requests()
         request = requests[0]
         self.assertEquals('asker', request.get_person())
         self.assertEquals('I have problems calling the rest API', request.get_message())
         self.assertEquals('link', request.get_success_condition())
-        self.assertEquals('https://blablabla.com/blablabla', request.get_data())
+        data = request.get_data()
+        self.assertEquals(2, len(data))
+        self.assertEquals('https://blablabla.com/blablabla', data[0])
+        self.assertEquals('https://second_url.com/second_url', data[1])
 
     def test_scenario_with_multiple_requests(self):
-        scenarios = get_scenarios_from_csv_file(scenario_file_path)
+        scenarios = get_scenarios_from_csv_file(SCENARIO_FILE_PATH)
         scenario = scenarios[1]
         self.assertEquals(2, len(scenario.get_requests()))
         requests = scenario.get_requests()
@@ -31,9 +34,11 @@ class TestScenarioReader(unittest.TestCase):
         self.assertEquals('asker', request_a.get_person())
         self.assertEquals('Hello', request_a.get_message())
         self.assertEquals('same', request_a.get_success_condition())
-        self.assertEquals('', request_a.get_data())
+        self.assertEquals([], request_a.get_data())
 
         self.assertEquals('agent', request_b.get_person())
         self.assertEquals('World', request_b.get_message())
         self.assertEquals('link', request_b.get_success_condition())
-        self.assertEquals('https://asdasd.com/asdasd', request_b.get_data())
+        data = request_b.get_data()
+        self.assertEquals(1, len(data))
+        self.assertEquals('https://asdasd.com/asdasd', data[0])
--- whisper_automatic_test/whisper_api_adapter.py
+++ whisper_automatic_test/whisper_api_adapter.py
@@ -1,5 +1,7 @@
+import json
 import requests
 
+from whisper_automatic_test.suggestion import Suggestion
 
 URL = 'https://whisper.us-east-1.elasticbeanstalk.com/whisper/suggestions'
 CHAT_KEY_IDENTIFIER = 'chatkey'
@@ -16,3 +18,8 @@ def get_json_for_whisper_api(request, chat_key):
 def get_suggestions_from_whisper_api(request, chat_key):
     response = requests.post(URL, json=get_json_for_whisper_api(request, chat_key))
     return response.content.decode("utf-8")
+
+
+def whisper_response_to_suggestions(whisper_response):
+    json_response = json.loads(whisper_response)
+    return [Suggestion('link', suggestion['printableUri']) for suggestion in json_response]
