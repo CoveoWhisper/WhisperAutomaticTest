15f132f39864e39171e46d0e65b507fbc735012f
Maxime Dupuis <webf0x@hotmail.com>
2018-04-14 13:23:43 -0400

    Print metrics and quality indexes from a CSV scenarios file (#12)

--- test/test_scenarios_runner.py
+++ test/test_scenarios_runner.py
@@ -8,8 +8,9 @@ from whisper_automatic_test.suggestion import Suggestion
 
 
 class TestScenariosRunner(unittest.TestCase):
-    def test_run_scenarios(self):
-        scenarios = [
+
+    def setUp(self):
+        self._scenarios = [
             Scenario(
                 [
                     Request('asker', 'hello', 'link', 'https://some_link.com'),
@@ -23,18 +24,20 @@ class TestScenariosRunner(unittest.TestCase):
             )
         ]
 
-        mock_get_suggestions = Mock()
-        mock_get_suggestions.side_effect = [
+        self._mock_get_suggestions = Mock()
+        self._mock_get_suggestions.side_effect = [
             [Suggestion('link', 'some_url'), Suggestion('link', 'another_url')],
             [Suggestion('link', 'url_of_a_different_suggestions_response')],
             [Suggestion('link', 'some_url_of_suggestion_scenario_2')]
         ]
 
-        mock_get_time = Mock()
-        mock_get_time.side_effect = [11, 22, 33, 44, 55, 66]
+        self._mock_get_time = Mock()
+        self._mock_get_time.side_effect = [11, 22, 33, 44, 55, 66]
 
-        scenario_runner = ScenariosRunner(mock_get_suggestions, mock_get_time)
-        suggestions_responses = scenario_runner.run(scenarios)
+        self._scenario_runner = ScenariosRunner(self._mock_get_suggestions, self._mock_get_time)
+
+    def test_run_scenarios(self):
+        suggestions_responses = self._scenario_runner.run(self._scenarios)
         self.assertEquals(3, len(suggestions_responses))
         suggestions_a = suggestions_responses[0].get_suggestions()
         self.assertEquals(2, len(suggestions_a))
@@ -58,3 +61,24 @@ class TestScenariosRunner(unittest.TestCase):
         self.assertEquals('some_url_of_suggestion_scenario_2', suggestions_c[0].get_data())
         self.assertEquals(55, suggestions_responses[2].get_timestamp_sent_request())
         self.assertEquals(66, suggestions_responses[2].get_timestamp_received_response())
+
+    def test_run_each_scenario_with_unique_chatkey(self):
+        self._scenario_runner.run(self._scenarios)
+
+        calls = self._mock_get_suggestions.call_args_list
+        first_call = calls[0][0]
+        first_request = first_call[0]
+        first_chatkey = first_call[1]
+        self.assertEquals('asker', first_request.get_person())
+        self.assertEquals('hello', first_request.get_message())
+        self.assertEquals('link', first_request.get_success_condition())
+        self.assertEquals(['https://some_link.com'], first_request.get_data())
+
+        second_call = calls[1][0]
+        second_chatkey = second_call[1]
+
+        third_call = calls[2][0]
+        third_chatkey = third_call[1]
+
+        self.assertEquals(first_chatkey, second_chatkey)
+        self.assertNotEquals(first_chatkey, third_chatkey)
---
+++ whisper_automatic_test/main.py
@@ -0,0 +1,88 @@
+import datetime
+import sys
+
+from whisper_automatic_test.metrics_analyzer import MetricsAnalyzer
+from whisper_automatic_test.quality_indexes_analyzer import QualityIndexesAnalyzer
+from whisper_automatic_test.scenario_reader import get_scenarios_from_csv_file
+from whisper_automatic_test.scenarios_runner import ScenariosRunner
+from whisper_automatic_test.whisper_api_adapter import get_suggestions_from_whisper_api
+
+
+def main():
+    arguments = sys.argv
+    assert 2 == len(arguments) and 'Missing argument: scenario CSV file path.'
+
+    csv_scenarios_file_path = arguments[1]
+    scenarios = get_scenarios_from_csv_file(csv_scenarios_file_path)
+    scenario_runner = ScenariosRunner(get_suggestions, get_time)
+    suggestions_responses = scenario_runner.run(scenarios)
+    metrics_analyzer = MetricsAnalyzer(scenarios, suggestions_responses)
+    quality_indexes_analyzer = QualityIndexesAnalyzer(metrics_analyzer)
+
+    print_metrics(metrics_analyzer)
+    print_quality_indexes(quality_indexes_analyzer)
+
+    pass
+
+
+def get_suggestions(request, chatkey):
+    return get_suggestions_from_whisper_api(request, chatkey)
+
+
+def get_time():
+    return datetime.datetime.utcnow()
+
+
+def print_metrics(metrics_analyzer):
+    metric_name_and_value_pairs = [
+        ('Average system response time', metrics_analyzer.calculate_average_system_response_time()),
+        ('Messages number', metrics_analyzer.calculate_messages_number()),
+        ('Mean position of chosen suggestions', metrics_analyzer.calculate_mean_position_of_chosen_suggestions()),
+        ('Total number of suggestions updates', metrics_analyzer.calculate_total_number_of_suggestions_updates()),
+        ('Number of unwanted suggestions updates', metrics_analyzer.calculate_number_of_unwanted_suggestions_updates()),
+        ('Number of selected suggestions', metrics_analyzer.calculate_number_of_selected_suggestions()),
+        ('Number of suggested questions', metrics_analyzer.calculate_number_of_suggested_questions()),
+        ('Number of suggested links', metrics_analyzer.calculate_number_of_suggested_links()),
+        (
+            'Mean confidence level of selected suggestions',
+            metrics_analyzer.calculate_mean_confidence_level_of_selected_suggestions
+        )
+    ]
+
+    print('Metrics')
+    print('=' * 80)
+    for metric_name_and_value_pair in metric_name_and_value_pairs:
+        metric_name = metric_name_and_value_pair[0]
+        metric_value = metric_name_and_value_pair[1]
+        print(metric_name, ": ", metric_value)
+
+
+def print_quality_indexes(quality_indexes_analyzer):
+    quality_index_name_and_value_pairs = [
+        ('Pertinence index', quality_indexes_analyzer.get_pertinence_index()),
+        ('Speed index', quality_indexes_analyzer.get_speed_index()),
+        (
+            'Relative confidence level accuracy index',
+            quality_indexes_analyzer.get_relative_confidence_level_accuracy_index()),
+        ('Intent accuracy index', quality_indexes_analyzer.get_intent_accuracy_index()),
+        ('Confidence index', quality_indexes_analyzer.get_confidence_index()),
+    ]
+    average_quality_index = (
+        sum(
+            quality_index_name_and_value_pair[1]
+            for quality_index_name_and_value_pair in quality_index_name_and_value_pairs
+        ) / len(quality_index_name_and_value_pairs)
+    )
+
+    print('Quality indexes')
+    print('=' * 80)
+    for quality_index_name_and_value_pair in quality_index_name_and_value_pairs:
+        quality_index_name = quality_index_name_and_value_pair[0]
+        quality_index_value = quality_index_name_and_value_pair[1]
+        print(quality_index_name, ": ", quality_index_value)
+
+    print('Average quality index: ', average_quality_index)
+
+
+if __name__ == "__main__":
+    main()
