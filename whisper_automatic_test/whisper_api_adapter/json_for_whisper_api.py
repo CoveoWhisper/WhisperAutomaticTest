eed592cfa9667f5193738c708c214ed9027558e5
MININT-F9QEIB9\CoveoPC <allec12b@gmail.com>
2018-11-17 14:07:41 -0500

    Add support for whisper version 14

--- whisper_automatic_test/whisper_api_adapter/json_for_whisper_api.py
+++ whisper_automatic_test/whisper_api_adapter/json_for_whisper_api.py
@@ -35,11 +35,16 @@ def _get_json_for_version12(request, chat_key):
     return _get_json_for_version11(request, chat_key)
 
 
+def _get_json_for_version14(request, chat_key):
+    return _get_json_for_version13(request, chat_key)
+
+
 _WHISPER_API_VERSION_TO_GET_JSON_FUNCTION = {
     '10': _get_json_for_version10,
     '11': _get_json_for_version11,
     '12': _get_json_for_version12,
-    '13': _get_json_for_version13
+    '13': _get_json_for_version13,
+    '14': _get_json_for_version14
 }
 
 
